eedf49567d528398fae70d8e2f7956b9b91b1134
Vikash2806 <vikashrajendran2806@gmail.com>
2023-01-05 12:34:04 +0530

    feat: conditionals add

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const app = Vue.createApp({
         "The Legend of King Arthur: The Sword in the Stone and the Lady of the Lake",
       author: "T. H. White",
       age: 90,
+      showBook: true,
     };
   },
   methods: {
     updateTitle(title) {
       this.title = title;
+    },
+    toggleBooks() {
+      this.showBook = !this.showBook;
     },
   },
 });
